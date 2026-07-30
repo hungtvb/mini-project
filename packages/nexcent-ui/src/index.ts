111f69cc70cfcf2c8d47002c0d804e2b64c16dc8
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-31 00:16:12 +0700

    refactor(ui): export Header and Footer

--- packages/nexcent-ui/src/index.ts
+++ packages/nexcent-ui/src/index.ts
@@ -3,6 +3,8 @@ export * from './components/Community/Community';
 export * from './components/ContactForm/ContactForm';
 export * from './components/Cta/Cta';
 export * from './components/Feature/Feature';
+export * from './components/Footer/Footer';
+export * from './components/Header/Header';
 export * from './components/Hero/Hero';
 export * from './components/Marketing/Marketing';
 export * from './components/Statistics/Statistics';
